7c6cfe6af2f2324571d4562682948606dfc0bd03
Antokolos <antokolos@gmail.com>
2019-08-03 12:37:07 +0600

    Used the Transition node to switch between states

--- scenes/female.gd
+++ scenes/female.gd
@@ -1,11 +1,11 @@
 extends Spatial
 
-func walk(look_angle_deg):
-	var angle = look_angle_deg if abs(look_angle_deg) < 90.0 else (90.0 if look_angle_deg > 0 else -90.0)
-	$AnimationTree.set("parameters/Blend2_Walk/blend_amount", 1.0)
-	$AnimationTree.set("parameters/Blend2_Head/blend_amount", 0.5 + 0.5 * (look_angle_deg / 90.0))
-
 func look(look_angle_deg):
 	var angle = look_angle_deg if abs(look_angle_deg) < 90.0 else (90.0 if look_angle_deg > 0 else -90.0)
-	$AnimationTree.set("parameters/Blend2_Walk/blend_amount", 0.0)
+	$AnimationTree.set("parameters/Transition/current", 0)
+	$AnimationTree.set("parameters/Blend2_Head/blend_amount", 0.5 + 0.5 * (look_angle_deg / 90.0))
+
+func walk(look_angle_deg):
+	var angle = look_angle_deg if abs(look_angle_deg) < 90.0 else (90.0 if look_angle_deg > 0 else -90.0)
+	$AnimationTree.set("parameters/Transition/current", 1)
 	$AnimationTree.set("parameters/Blend2_Head/blend_amount", 0.5 + 0.5 * (look_angle_deg / 90.0))
