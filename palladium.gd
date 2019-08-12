ad07b4bec0179e2ba0180581e3a4b57635099b30
Antokolos <antokolos@gmail.com>
2019-08-12 22:26:31 +0600

    Player toggling

--- palladium.gd
+++ palladium.gd
@@ -1,5 +1,19 @@
 extends Navigation
 
+var player_toggle_enable = false
+
 func _ready():
 	$player.take("saffron_bun", null)
 	TranslationServer.set_locale("ru")
+
+func _unhandled_input(event):
+	if not player_toggle_enable:
+		return
+	var player = get_node("player")
+	var player_female = get_node("player_female")
+	if event is InputEventKey:
+		match event.scancode:
+			KEY_9:
+				player.become_player()
+			KEY_0:
+				player_female.become_player()
