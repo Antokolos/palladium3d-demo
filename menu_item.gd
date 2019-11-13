7a6f71f4e1c1cc3225283bd661be55bda1859c67
Antokolos <antokolos@gmail.com>
2019-11-14 04:37:04 +0600

    Small menu corrections

--- menu_item.gd
+++ menu_item.gd
@@ -3,6 +3,8 @@ class_name MenuItem
 
 export var text_path = "Text023"
 export var sfx_player_over_path = "../../SFXOver"
+export var emission_enabled = true
+export var emission_energy = 0.05
 onready var menu_item_normal = SpatialMaterial.new()
 onready var menu_item_highlight = SpatialMaterial.new()
 onready var text_node = get_node(text_path)
@@ -12,6 +14,10 @@ func _ready():
 	menu_item_normal.set("albedo_color", Color("#FFFFFF"))
 	menu_item_normal.set("metallic", 0.9)
 	menu_item_normal.set("roughness", 0.1)
+	menu_item_normal.set("emission_enabled", emission_enabled)
+	if emission_enabled:
+		menu_item_normal.set("emission", Color("#FFFFFF"))
+		menu_item_normal.set("emission_energy", emission_energy)
 	menu_item_highlight.set("albedo_color", Color("#EC6418"))
 	menu_item_highlight.set("metallic", 0.1)
 	menu_item_highlight.set("roughness", 0.9)
