992ae1f20ae3b4873aa70b56626a0f678bd7cc26
Antokolos <antokolos@gmail.com>
2020-02-09 23:01:20 +0600

    Male fixes

--- scenes/transition_palladium.gd
+++ scenes/transition_palladium.gd
@@ -1,7 +1,7 @@
 extends StaticBody
 
 func use(player_node):
-	game_params.scene_path = "res://palladium_full.tscn"
+	game_params.scene_path = "res://palladium.tscn"
 	get_tree().change_scene("res://scene_loader.tscn")
 
 func add_highlight(player_node):
