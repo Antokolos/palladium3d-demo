e705095ec6a6ac317f7ce8872f78100ecb131120
Antokolos <antokolos@gmail.com>
2020-04-08 16:26:53 +0600

    New/deleted files

---
+++ viewport.gd
@@ -0,0 +1,15 @@
+extends Viewport
+
+func _ready():
+	set_size_override_stretch(true)
+	get_viewport().connect("size_changed", self, "_root_viewport_size_changed")
+	settings.connect("resolution_changed", self, "_on_resolution_changed")
+	_on_resolution_changed(settings.resolution)
+	
+func _root_viewport_size_changed():
+	_on_resolution_changed(settings.resolution)
+
+func _on_resolution_changed(ID):
+	var size = settings.get_resolution_size(ID)
+	self.size = size
+	self.set_size_override(true, size)
