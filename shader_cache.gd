c4e635b00e4205058b672e02c7a273367b52c28d
Antokolos <antokolos@gmail.com>
2020-04-13 16:40:52 +0600

    Messing with shader cache again

--- shader_cache.gd
+++ shader_cache.gd
@@ -3,130 +3,153 @@ class_name ShaderCache
 
 const SHADER_CACHE_ENABLED = true
 const SHADER_CACHE_HIDING_ENABLED = true
-const SHADER_CACHE_IGNORE_SKELETONS = false
-const STEP = 0.02
-const HALFROW = 10
-
-onready var holder1 = get_node("shader_cache_holder1")
-onready var holder2 = get_node("shader_cache_holder2")
+const STEP = 0.005
+const HALFROW = 20
 
 var stage = 0
+var rids = {}
+var skeleton_paths = {}
+var character_skeleton_paths = {}
 
 func _ready():
 	#refresh() -- will be called on settings ready
 	pass
 
-func get_cacheable_items(scn, ignore_skeletons):
+func get_cacheable_items(scn):
 	var result = []
 	if scn is MeshInstance:
-		var sk = null if ignore_skeletons else scn.get_node(scn.get_skeleton_path())
-		# Adding materials from MeshInstance's surfaces, if any
-		var sc = scn.get_surface_material_count()
-		for i in range(0, sc):
-			var mat = scn.get_surface_material(i)
-			if mat:
-				result.append({"material": mat, "skeleton": sk if sk is Skeleton else null, "particles_material": null})
-		# Adding materials from MeshInstance's meshes' surfaces, if any
-		var mesh = scn.mesh
-		if mesh and mesh is Mesh:
-			var sc_mesh = mesh.get_surface_count()
-			for j in range(0, sc_mesh):
-				var mat = mesh.surface_get_material(j)
-				if mat:
-					result.append({"material": mat, "skeleton": sk if sk is Skeleton else null, "particles_material": null})
+		result.append({"mesh" : scn, "particles": null})
 	elif scn is Particles:
-		var pmat = scn.get_process_material()
-		# TODO: add another draw pass materials if they will be used
-		var mesh = scn.draw_pass_1
-		if mesh and mesh is Mesh:
-			var sc_mesh = mesh.get_surface_count()
-			for j in range(0, sc_mesh):
-				var mat = mesh.surface_get_material(j)
-				if mat:
-					result.append({"material": mat, "skeleton": null, "particles_material": pmat})
+		result.append({"mesh" : null, "particles": scn})
 
 	for ch in scn.get_children():
-		var items = get_cacheable_items(ch, ignore_skeletons)
+		var items = get_cacheable_items(ch)
 		for item in items:
 			result.append(item)
 	return result
 
-func make_asset(pos, material, skeleton_path):
-	var skeleton = get_node(skeleton_path).duplicate(Node.DUPLICATE_USE_INSTANCING) if skeleton_path else null
-	if skeleton:
-		skeleton.set_transform(Transform())
-		var aabb = AABB(Vector3(0, 0, 0), Vector3(STEP, STEP, STEP))
-		for skeleton_child in skeleton.get_children():
-			if skeleton_child is VisualInstance:
-				aabb = aabb.merge(skeleton_child.get_aabb())
-		var size = aabb.size
-		skeleton.set_scale(Vector3(STEP / (2 * size.x), STEP / (2 * size.y), STEP / (2 * size.z)))
-		holder1.add_child(skeleton)
-		skeleton.global_translate(Vector3(pos.x, pos.y, 0))
-		for ch in skeleton.get_children():
-			if ch is AnimationPlayer:
-				var anim_list = ch.get_animation_list()
-				for anim in anim_list:
-					ch.play(anim)
-	else:
-		var asset = MeshInstance.new()
-		var mname = material.get_name()
-		if not mname.empty():
-			asset.set_name(mname)
-		asset.mesh = SphereMesh.new()
-		asset.mesh.radius = STEP / 4.0
-		asset.mesh.height = STEP / 2.0
-		asset.mesh.set_material(material)
-		holder1.add_child(asset)
-		asset.translate(Vector3(pos.x, pos.y, 0))
-	pos.x = pos.x + STEP
-	if pos.x > STEP * HALFROW:
-		pos.x = -STEP * HALFROW
-		pos.y = pos.y + STEP
-	return pos
+func get_scale_from_aabb(aabb):
+	var size = aabb.size
+	if size.x == 0:
+		size.x = 0.001
+	if size.y == 0:
+		size.y = 0.001
+	if size.z == 0:
+		size.z = 0.001
+	return Vector3(STEP / (2 * size.x), STEP / (2 * size.y), STEP / (2 * size.z))
 
-func make_particles(pos, particles_material, material):
-	var particles = Particles.new()
-	particles.set_name(particles_material.get_name() + "_" + material.get_name())
-	particles.draw_pass_1 = SphereMesh.new()
-	particles.draw_pass_1.radius = STEP / 4.0
-	particles.draw_pass_1.height = STEP / 2.0
-	holder1.add_child(particles)
+func make_skeleton(pos, skeleton):
+	skeleton.set_transform(Transform())
+	skeleton.translate(Vector3(pos.x, pos.y, 0))
+	var aabb = AABB(Vector3(0, 0, 0), Vector3(STEP, STEP, STEP))
+	for skeleton_child in skeleton.get_children():
+		if skeleton_child is VisualInstance:
+			aabb = aabb.merge(skeleton_child.get_aabb())
+	skeleton.set_scale(get_scale_from_aabb(aabb))
+	for ch in skeleton.get_children():
+		if ch is AnimationPlayer:
+			var anim_list = ch.get_animation_list()
+			for anim in anim_list:
+				ch.play(anim)
+	return skeleton
+
+func make_base_asset(pos, resource):
+	var asset = MeshInstance.new()
+	asset.set_transform(Transform())
+	asset.translate(Vector3(pos.x, pos.y, 0))
+	var mname = resource.get_name() if resource else "" 
+	if not mname.empty():
+		asset.set_name(mname)
+	return asset
+
+func make_asset(pos, material):
+	var asset = make_base_asset(pos, material)
+	asset.mesh = SphereMesh.new()
+	asset.mesh.radius = STEP / 4.0
+	asset.mesh.height = STEP / 2.0
+	asset.mesh.set_material(material)
+	return asset
+
+func make_mesh(pos, mesh, surface_materials = []):
+	var asset = make_base_asset(pos, mesh)
+	asset.mesh = mesh
+	var aabb = AABB(Vector3(0, 0, 0), Vector3(STEP, STEP, STEP))
+	aabb = aabb.merge(asset.get_aabb())
+	asset.set_scale(get_scale_from_aabb(aabb))
+	for i in range(surface_materials.size()):
+		asset.set_surface_material(i, surface_materials[i])
+	return asset
+
+func make_particles(pos, particles):
+	particles.set_transform(Transform())
 	particles.translate(Vector3(pos.x, pos.y, 0))
-	pos.x = pos.x + STEP
-	if pos.x > STEP * HALFROW:
-		pos.x = -STEP * HALFROW
-		pos.y = pos.y + STEP
-	particles.draw_pass_1.set_material(material)
-	particles.set_process_material(particles_material)
+	var aabb = AABB(Vector3(0, 0, 0), Vector3(STEP, STEP, STEP))
+	aabb = aabb.merge(particles.get_aabb())
+	particles.set_scale(get_scale_from_aabb(aabb))
 	particles.emitting = true
 	particles.restart()
+	return particles
+
+func next_pos(pos):
+	pos.x = pos.x + STEP
+	if pos.x > STEP * HALFROW:
+		pos.x = -STEP * HALFROW
+		pos.y = pos.y + STEP
 	return pos
 
 func add_material_meshes(pos, scn):
+	var items = get_cacheable_items(scn)
 	pos.x = pos.x + STEP / 2.0
 	pos.y = pos.y + STEP / 2.0
-	var items = get_cacheable_items(scn, SHADER_CACHE_IGNORE_SKELETONS)
-	var rids = {}
-	var skeleton_paths = {}
 	for item in items:
-		var material = item["material"]
-		var skeleton = item["skeleton"]
-		var particles_material = item["particles_material"]
-		var id = material.get_rid().get_id()
-		if not rids.has(id):
-			rids[id] = true
-			pos = make_asset(pos, material, null)
-		if skeleton:
-			var skeleton_path = skeleton.get_path()
-			if not skeleton_paths.has(skeleton_path):
-				skeleton_paths[skeleton_path] = true
-				pos = make_asset(pos, material, skeleton_path)
-		if particles_material:
-			var pid = particles_material.get_rid().get_id()
-			if not rids.has(pid):
-				rids[pid] = true
-				pos = make_particles(pos, particles_material, material)
+		var mesh = item["mesh"]
+		var particles = item["particles"]
+		if mesh:
+			# Adding materials from MeshInstance's surfaces, if any
+			var sc = mesh.get_surface_material_count()
+			var surface_materials = []
+			for i in range(0, sc):
+				var mat = mesh.get_surface_material(i)
+				if mat:
+					surface_materials.append(mat)
+					var rid = mat.get_rid().get_id()
+					if not rids.has(rid):
+						rids[rid] = make_asset(pos, mat)
+						self.add_child(rids[rid])
+						pos = next_pos(pos)
+			# Adding materials from MeshInstance's meshes' surfaces, if any
+			var m = mesh.mesh
+			if m and m is Mesh:
+				var sc_mesh = m.get_surface_count()
+				for j in range(0, sc_mesh):
+					var mat = m.surface_get_material(j)
+					if mat:
+						var rid = mat.get_rid().get_id()
+						if not rids.has(rid):
+							rids[rid] = make_asset(pos, mat)
+							self.add_child(rids[rid])
+							pos = next_pos(pos)
+			
+			var mesh_id = m.get_rid().get_id()
+			var sk = mesh.get_node(mesh.get_skeleton_path())
+			if sk is Skeleton:
+				var sk_path = sk.get_path()
+				var target_dic = character_skeleton_paths if sk.get_parent() is PalladiumCharacter else skeleton_paths
+				if not target_dic.has(sk_path):
+					target_dic[sk_path] = make_skeleton(pos, sk.duplicate(Node.DUPLICATE_USE_INSTANCING))
+					self.add_child(target_dic[sk_path])
+					pos = next_pos(pos)
+			if not rids.has(mesh_id):
+				rids[mesh_id] = make_mesh(pos, m, surface_materials)
+				self.add_child(rids[mesh_id])
+				pos = next_pos(pos)
+		elif particles:
+			var pmat = particles.get_process_material()
+			var rid = pmat.get_rid().get_id()
+			if not rids.has(rid):
+				rids[rid] = make_particles(pos, particles.duplicate(Node.DUPLICATE_USE_INSTANCING))
+				self.add_child(rids[rid])
+				pos = next_pos(pos)
 	return pos
 
 func _process(delta):
@@ -136,9 +159,10 @@ func _process(delta):
 		1:
 			stage = stage + 1
 			game_params.get_hud().set_message("Please wait...")
-			for node in holder1.get_children():
-				node.queue_free()
-			for node in holder2.get_children():
+			rids.clear()
+			skeleton_paths.clear()
+			character_skeleton_paths.clear()
+			for node in self.get_children():
 				node.queue_free()
 			get_tree().call_group("room_enablers", "set_active", false)
 		2:
@@ -150,9 +174,20 @@ func _process(delta):
 			# Show all items for one frame
 		_:
 			if SHADER_CACHE_HIDING_ENABLED:
-				for node in holder1.get_children():
-					holder1.remove_child(node)
-					holder2.add_child(node)
+				for rid in rids.keys():
+					var a = rids[rid]
+					#a.visible = false
+					self.remove_child(a)
+				for sk_path in skeleton_paths.keys():
+					var sk = skeleton_paths[sk_path]
+					#sk.visible = false
+					self.remove_child(sk)
+				for character_sk_path in character_skeleton_paths.keys():
+					var character_sk = character_skeleton_paths[character_sk_path]
+					# Moving all character skeletons to center
+					var scale = character_sk.get_scale()
+					character_sk.set_transform(Transform())
+					character_sk.set_scale(scale)
 			game_params.get_hud().clear_message()
 			get_tree().call_group("room_enablers", "set_active", true)
 			stage = 0
