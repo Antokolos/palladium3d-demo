c47dfa173c4ee8a9994c89a028211a2a9f174103
Antokolos <antokolos@gmail.com>
2020-04-16 18:05:53 +0600

    Dialog system targets concept was reworked

---
+++ cutscene_manager.gd
@@ -0,0 +1,65 @@
+extends Node
+class_name CutsceneManager
+
+var cutscene_node = null
+var conversation_name = null
+var target = null
+var is_cutscene = false
+
+func _ready():
+	conversation_manager.connect("conversation_finished", self, "_on_conversation_finished")
+
+func _on_conversation_finished(player, conversation_name, target, initiator):
+	if not self.target and target:
+		return
+	if self.target and not target:
+		return
+	if self.conversation_name == conversation_name:
+		if (not self.target and not target) or self.target.name_hint == target.name_hint:
+			stop_cutscene(player)
+
+func start_cutscene(player, cutscene_node, conversation_name = null, target = null):
+	is_cutscene = true
+	self.conversation_name = conversation_name
+	self.target = target
+	borrow_camera(player, cutscene_node)
+
+func stop_cutscene(player):
+	is_cutscene = false
+	self.conversation_name = null
+	self.target = null
+	restore_camera(player)
+
+func borrow_camera(player, cutscene_node):
+	var player_camera_holder = player.get_cam_holder()
+	if player_camera_holder.get_child_count() == 0:
+		# Do nothing if player has no camera. It looks like we are in cutscene already.
+		return
+	var camera = player_camera_holder.get_child(0)
+	camera.enable_use(false)
+	self.cutscene_node = cutscene_node
+	if not cutscene_node:
+		return
+	player.reset_rotation()
+	player.set_simple_mode(false)
+	player_camera_holder.remove_child(camera)
+	cutscene_node.add_child(camera)
+
+func restore_camera(player):
+	var player_camera_holder = player.get_cam_holder()
+	if not cutscene_node:
+		var camera = player_camera_holder.get_child(0)
+		camera.enable_use(true)
+		return
+	player.set_simple_mode(true)
+	var camera = cutscene_node.get_child(0)
+	cutscene_node.remove_child(camera)
+	player_camera_holder.add_child(camera)
+	camera.enable_use(true)
+	cutscene_node = null
+
+func get_cam():
+	return cutscene_node.get_child(0) if cutscene_node and cutscene_node.get_child_count() > 0 else null
+
+func is_cutscene():
+	return is_cutscene
---
+++ lipsync_manager.gd
@@ -0,0 +1,95 @@
+extends Node
+class_name LipsyncManager
+
+const VOWELS = ["А", "Е", "Ё", "И", "О", "У", "Ы", "Э", "Ю", "Я"]
+const CONSONANTS =           ["Б", "В", "Г", "Д", "Ж", "З", "К", "Л", "М", "Н", "П", "Р", "С", "Т", "Ф", "Х", "Ц", "Ч", "Ш", "Щ"]
+const CONSONANTS_EXCLUSIONS =[          "Г", "Д",           "К",           "Н",      "Р",      "Т",      "Х"]
+const SPECIALS = ["Ь", "Ъ", "Й"]
+const STOPS = [".", "!", "?", ";", ":"]
+const MINIMUM_AUTO_ADVANCE_TIME_SEC = 1.8
+
+func get_conversation_sound_path(conversation_name, target_name_hint = null):
+	var locale = "ru" if settings.vlanguage == settings.VLANGUAGE_RU else ("en" if settings.vlanguage == settings.VLANGUAGE_EN else null)
+	if not locale:
+		return null
+	var csp = "sound/dialogues/%s/%s/%s/" % [locale, target_name_hint if target_name_hint else "root", conversation_name]
+	var dir = Directory.new()
+	return csp if dir.dir_exists(csp) else null
+
+func play_sound_and_start_lipsync(character, conversation_name, target_name_hint, file_name, text = null, transcription = null):
+	var phonetic = transcription if transcription else (text_to_phonetic(text.strip_edges()) if text else null)
+	var conversation_sound_path = get_conversation_sound_path(conversation_name, target_name_hint)
+	if not conversation_sound_path:
+		return false
+	var ogg_file = File.new()
+	ogg_file.open(conversation_sound_path + file_name, File.READ)
+	var bytes = ogg_file.get_buffer(ogg_file.get_len())
+	var stream = AudioStreamOGGVorbis.new()
+	stream.data = bytes
+	var length = stream.get_length()
+	$ShortPhraseTimer.wait_time = 0.01 if length >= MINIMUM_AUTO_ADVANCE_TIME_SEC else MINIMUM_AUTO_ADVANCE_TIME_SEC - length
+	$AudioStreamPlayer.stream = stream
+	$AudioStreamPlayer.play()
+	if phonetic:
+		#print(phonetic)
+		character.get_model().speak_text(phonetic, length)
+	return true
+
+func letter_vowel(letter):
+	return VOWELS.has(letter.to_upper())
+
+func letter_consonant(letter):
+	return CONSONANTS.has(letter.to_upper())
+
+func letter_stop(letter):
+	return STOPS.has(letter)
+
+func letter_skip(letter, use_exclusions):
+	var l = letter.to_upper()
+	return SPECIALS.has(l) or (use_exclusions and CONSONANTS_EXCLUSIONS.has(l)) or not (VOWELS.has(l) or CONSONANTS.has(l))
+
+func letter_to_phonetic(letter, use_exclusions):
+	var l = letter.to_upper()
+	if letter_stop(l):
+		return "..."
+	if letter_skip(l, use_exclusions):
+		return ""
+	match l:
+		"Е":
+			return "Э"
+		"Ё":
+			return "О"
+		"Ю":
+			return "У"
+		"Я":
+			return "А"
+		_:
+			return letter
+
+func text_to_phonetic(text):
+	var result = ""
+	var words = text.split(" ", false)
+	for word in words:
+		var i = 0
+		var wl = word.length()
+		var use_exclusions = false
+		while i < wl:
+			var letter = word[i]
+			result = result + letter_to_phonetic(letter, use_exclusions)
+			use_exclusions = true
+			i = i + 1
+		result = result + " "
+	return result
+
+func _on_AudioStreamPlayer_finished():
+	var player = game_params.get_player()
+	if conversation_manager.in_choice:
+		conversation_manager.story_proceed(player)
+	elif StoryNode.CanChoose():
+		var ch = StoryNode.GetChoices(TranslationServer.get_locale())
+		if ch.size() == 1:
+			$ShortPhraseTimer.start()
+
+func _on_ShortPhraseTimer_timeout():
+	var player = game_params.get_player()
+	conversation_manager.story_choose(player, 0)
