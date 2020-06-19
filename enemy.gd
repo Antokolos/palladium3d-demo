e6adce91030434a6b96ef95bd500324f6de56a30
Antokolos <antokolos@gmail.com>
2020-06-19 23:17:10 +0600

    Enemy scripts

--- enemy.gd
+++ enemy.gd
@@ -1,8 +1,37 @@
 extends PLDCharacter
 class_name PLDEnemy
 
+var activated = false
+
 func _ready():
 	pass
 
+### Use target ###
+
+func add_highlight(player_node):
+	return "E: Disable" if activated else "E: Activate"
+
+func remove_highlight(player_node):
+	pass
+
+func use(player_node):
+	activate(not activated)
+
+func activate(enable):
+	get_model().activate(enable)
+	activated = enable
+
+func get_preferred_target():
+	return game_params.get_player() if activated else null
+
+func is_enemy():
+	return true
+
+func attack():
+	get_model().attack()
+
 func _on_CutsceneTimer_timeout():
 	set_look_transition(true)
+
+func _physics_process(delta):
+	pass
--- palladium.gd
+++ palladium.gd
@@ -7,6 +7,7 @@ func do_init(is_loaded):
 	player.set_sound_walk(player.SoundId.SOUND_WALK_CONCRETE)
 	player_female.set_sound_walk(player.SoundId.SOUND_WALK_CONCRETE)
 	player_bandit.set_sound_walk(player.SoundId.SOUND_WALK_CONCRETE)
+	get_node("greek_skeleton").set_sound_walk(player.SoundId.SOUND_WALK_CONCRETE)
 	if not conversation_manager.conversation_is_in_progress("004_TorchesIgnition") and conversation_manager.conversation_is_not_finished("004_TorchesIgnition"):
 		get_tree().call_group("torches", "enable", false, false)
 	game_params.connect("shader_cache_processed", self, "_on_shader_cache_processed")
