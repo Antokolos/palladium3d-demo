5c31c5f5a85e220450ab086be474920c172eb1b7
Antokolos <antokolos@gmail.com>
2020-08-19 03:46:29 +0600

    Hinged door

--- addons/palladium/core/environment/door.gd
+++ addons/palladium/core/environment/door.gd
@@ -8,12 +8,12 @@ const ANIM_SPEED_SCALE = 0.725
 export(DB.DoorIds) var door_id = DB.DoorIds.NONE
 export var initially_opened = false
 export var reverse = false
-export var door_collision_path = "StaticBody/CollisionShape"
+export var door_body_path = "StaticBody"
 export var anim_player_path = "door_4_armature001/AnimationPlayer"
 export var anim_name_open = "door_4_armatureAction.001"
 
 onready var anim_player = get_node(anim_player_path)
-onready var door_collision = get_node(door_collision_path)
+onready var door_body = get_node(door_body_path)
 
 func _ready():
 	anim_player.connect("animation_finished", self, "_on_animation_finished")
@@ -22,23 +22,30 @@ func _ready():
 func _on_animation_finished(anim_name):
 	emit_signal("door_state_changed", door_id, is_opened())
 
+func enable_collisions(body, enable):
+	for collision in body.get_children():
+		if collision is CollisionShape:
+			collision.disabled = not enable
+
 func open(with_sound = true, force = false):
 	if not force and is_opened():
-		return
+		return false
 	anim_player.play(anim_name_open, -1, -ANIM_SPEED_SCALE if reverse else ANIM_SPEED_SCALE, reverse)
-	door_collision.disabled = true
+	enable_collisions(door_body, false)
 	game_state.set_door_state(get_path(), true)
 	if with_sound and has_node("door_sound"):
 		get_node("door_sound").play(false)
+	return true
 
 func close(with_sound = true, force = false):
 	if not force and not is_opened():
-		return
+		return false
 	if with_sound and has_node("door_sound"):
 		get_node("door_sound").play(true)
 	anim_player.play(anim_name_open, -1, ANIM_SPEED_SCALE if reverse else -ANIM_SPEED_SCALE, not reverse)
 	game_state.set_door_state(get_path(), false)
-	door_collision.disabled = false
+	enable_collisions(door_body, true)
+	return true
 
 func is_opened():
 	var state = game_state.get_door_state(get_path())
---
+++ addons/palladium/core/environment/door_hinged.gd
@@ -0,0 +1,23 @@
+extends PLDDoor
+class_name PLDDoorHinged
+
+export(NodePath) var door_body_opened_path = NodePath("StaticBodyOpened")
+
+var door_body_opened = null
+
+func get_door_body_opened():
+	if not door_body_opened:
+		door_body_opened = get_node(door_body_opened_path)
+	return door_body_opened
+
+func open(with_sound = true, force = false):
+	if not .open(with_sound, force):
+		return false
+	enable_collisions(get_door_body_opened(), true)
+	return true
+
+func close(with_sound = true, force = false):
+	if not .close(with_sound, force):
+		return false
+	enable_collisions(get_door_body_opened(), false)
+	return true
