6f55e7e5ebde5232467423e21797c55bc14beb04
Antokolos <antokolos@gmail.com>
2020-11-06 22:26:45 +0600

    Sync with main

--- addons/palladium/core/environment/riddle.gd
+++ addons/palladium/core/environment/riddle.gd
@@ -2,9 +2,11 @@ extends Spatial
 class_name PLDRiddle
 
 signal riddle_error(riddle)
+signal riddle_secret(riddle)
 signal riddle_success(riddle)
 
 export(DB.RiddleIds) var riddle_id = DB.RiddleIds.NONE
 
 func connect_signals(target):
+	connect("riddle_secret", target, "_on_riddle_secret")
 	connect("riddle_success", target, "_on_riddle_success")
