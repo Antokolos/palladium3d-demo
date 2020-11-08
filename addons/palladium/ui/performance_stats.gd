de88ffa4cfae0f96e5419585613ab52d979264ee
Antokolos <antokolos@gmail.com>
2020-11-08 17:09:38 +0600

    Sync with main

--- addons/palladium/ui/performance_stats.gd
+++ addons/palladium/ui/performance_stats.gd
@@ -15,6 +15,13 @@ func _physics_process(delta):
 	visible = true
 	var viewport = game_state.get_viewport()
 	var viewport_size = viewport.size if viewport else Vector2(0, 0)
+	var active_torches = 0
+	var inactive_torches = 0
+	for t in get_tree().get_nodes_in_group("torches"):
+		if t.is_visible_in_tree():
+			active_torches = active_torches + 1
+		else:
+			inactive_torches = inactive_torches + 1
 	var stats = [
 	"FPS: %d" % Performance.get_monitor(Performance.TIME_FPS),
 	"\nRender objects: %d" % Performance.get_monitor(Performance.RENDER_OBJECTS_IN_FRAME),
@@ -22,6 +29,8 @@ func _physics_process(delta):
 	"\nShader changes: %d" % Performance.get_monitor(Performance.RENDER_SHADER_CHANGES_IN_FRAME),
 	"\nSurface changes: %d" % Performance.get_monitor(Performance.RENDER_SURFACE_CHANGES_IN_FRAME),
 	"\nDrawcalls: %d" % Performance.get_monitor(Performance.RENDER_DRAW_CALLS_IN_FRAME),
+	"\nTorches active: %d" % active_torches,
+	"\nTorches inactive: %d" % inactive_torches,
 	"\nCutoff enabled: %s" % ("true" if settings.cutoff_enabled else "false"),
 	"\nCamera Far: %f" % camera.far if camera else "",
 	"\nViewport size: %dx%d" % [viewport_size.x, viewport_size.y]
