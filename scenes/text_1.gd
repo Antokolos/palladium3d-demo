38cc04bfdd757959d250718bb582ed080e1f29ed
Antokolos <antokolos@gmail.com>
2021-06-03 05:25:06 +0600

    Improvement (READ_ASK)

--- scenes/text_1.gd
+++ scenes/text_1.gd
@@ -4,4 +4,4 @@ func use(player_node, camera_node):
 	conversation_manager.start_area_conversation("010-1-2_MusesHint")
 
 func get_usage_code(player_node):
-	return "" if conversation_manager.conversation_is_in_progress() or conversation_manager.conversation_is_finished("010-1-2_MusesHint") else "ACTION_READ"
+	return "" if conversation_manager.conversation_is_in_progress() or conversation_manager.conversation_is_finished("010-1-2_MusesHint") else "ACTION_READ_ASK"
